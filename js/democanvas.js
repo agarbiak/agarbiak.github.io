bc65e1775b487cd12ce770f5311207c41cc0af50
Alexander Garbiak <garbiak.personal@gmail.com>
2020-04-30 21:14:34 +0100

    rebuilding site 30 Apr 2020 21:14:33

---
+++ js/democanvas.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('demoCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'blue';
+// (x postion, y postion, width, height)
+ctx.fillRect(10, 10, 150, 100);
