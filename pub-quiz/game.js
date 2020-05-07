a2de3cba73681dbb0618f9c5b4eebdf42d2a1288
Alexander Garbiak <garbiak.personal@gmail.com>
2020-05-07 10:11:43 +0100

    rebuilding site 07 May 2020 10:11:42

--- pub-quiz/game.js
+++ pub-quiz/game.js
@@ -15,7 +15,7 @@ let availableQuestions = [];
 let questions = [];
 
 fetch(
-    "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple"
+    "https://opentdb.com/api.php?amount=10&category=14&difficulty=easy&type=multiple"
     )
     .then( res => {
         return res.json();
