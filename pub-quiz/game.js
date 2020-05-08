dd7b4d7d677caf15079a636af9371d5e69b6901e
Alexander Garbiak <garbiak.personal@gmail.com>
2020-05-08 20:37:55 +0100

    rebuilding site 08 May 2020 20:37:55

--- pub-quiz/game.js
+++ pub-quiz/game.js
@@ -5,6 +5,8 @@ const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
 const loader = document.getElementById("loader");
 const game = document.getElementById("game");
+const pictureType = document.getElementById("pictureType");
+const audioType = document.getElementById("audioType");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -12,10 +14,15 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
+var imgElement = document.createElement('img');
+let imgSource = "/pub-quiz/img/";
+
+var audioElement = document.createElement('audio');
+let audioSource = "/pub-quiz/audio/";
+
 let questions = [];
 
 fetch(
-    // "https://opentdb.com/api.php?amount=10&category=14&difficulty=easy&type=multiple"
     "/pub-quiz/questions-today.json"
     )
     .then( res => {
@@ -24,9 +31,10 @@ fetch(
     .then(loadedQuestions => {
         questions = loadedQuestions.results.map( loadedQuestion => {
            const formattedQuestion = {
-               question: loadedQuestion.question
+               question: loadedQuestion.question,
+               metaType: loadedQuestion.meta,
+               metaSrc: loadedQuestion.metaSrc
            };
-
            const answerChoices = [...loadedQuestion.incorrect_answers];
            formattedQuestion.answer = Math.floor(Math.random()*3) + 1;
            answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
@@ -34,7 +42,7 @@ fetch(
            answerChoices.forEach((choice, index) => {
                formattedQuestion["choice" + (index+1)] = choice;
            })
-
+        //    console.log(formattedQuestion);   
            return formattedQuestion;
        })
        startGame();
@@ -71,6 +79,30 @@ getNewQuestion = () => {
     currentQuestion = availableQuestions[questionIndex];
     question.innerHTML = currentQuestion.question;
 
+    //Set picture type question
+    if(currentQuestion.metaType === "Picture"){
+        audioType.classList.add("hidden");
+        imgElement.setAttribute("src", imgSource.concat(currentQuestion.metaSrc));
+        imgElement.setAttribute("alt", currentQuestion.metaSrc);
+        document.getElementById("pictureType").appendChild(imgElement);
+        pictureType.classList.remove("hidden");
+    } 
+    // Set audio type question
+    else if(currentQuestion.metaType === "Audio") {
+        pictureType.classList.add("hidden");        
+        audioElement.setAttribute("src", audioSource.concat(currentQuestion.metaSrc));
+        audioElement.setAttribute("controls", "controls");
+        audioElement.setAttribute("controlsList", "nodownload");
+        document.getElementById("audioType").appendChild(audioElement);
+        audioType.classList.remove("hidden");
+    }
+    // Set basic question
+     else {
+        console.log("Question is a non-picture round");
+        // document.getElementById("pictureType").removeChild(imgElement);
+        pictureType.classList.add("hidden");
+        audioType.classList.add("hidden");
+    }
     choices.forEach(choice => {
         const number = choice.dataset["number"];
         choice.innerHTML = currentQuestion["choice" + number];
